f8dee3167e7e4254337cd85a71dd79339bba27ce
amkhrjee <aniruddha.mukherji@outlook.com>
2025-06-26 18:00:36 +0530

    fix: enhance about page content and clarify project details

--- about_page.py
+++ about_page.py
@@ -9,8 +9,16 @@ Learn more about the project.
 [Learn more about me](https://amkhrjee.in)
 """)
 
-st.sidebar.divider()
 st.markdown("""
-This is an experimental project built with LangChain.
+This is an experimental RAG project built with LangChain. The AI here is a [ReAct agent](https://arxiv.org/abs/2210.03629) connected to a MongoDB vector database.
+            
+The agent retrieves data from a text file stored and indexed in the database as necessary. 
+            
+The agent gets its behaviour from a straight-forward system prompt.
+            
+This project is not perfect, and might hallucinate and reveal the identity of the foundation model used, which, by the way, is Gemini 2.0-flash.
 
+Thus, this is still under active development and I intent to add more features in the future.
+            
+Till then, have fun!            
 """)
